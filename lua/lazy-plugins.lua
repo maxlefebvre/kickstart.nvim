c6479eed1c2227396023a488bee51dbdf44737e7
Max Lefebvre <lefebvremaximillian@gmail.com>
2026-02-20 09:21:47 -0500

    add themes folder and onedarkh

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -35,8 +35,6 @@ require('lazy').setup({
 
   require 'kickstart.plugins.blink-cmp',
 
-  require 'kickstart.plugins.tokyonight',
-
   require 'kickstart.plugins.todo-comments',
 
   require 'kickstart.plugins.mini',
@@ -60,8 +58,8 @@ require('lazy').setup({
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
+  { import = 'custom.themes' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
